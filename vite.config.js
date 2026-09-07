8519f753281d5632bb3a037eb574d6b7cb40412b
rockinhera <rockinheraa@gmail.com>
2026-09-07 22:45:47 +0500

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/smart-dev-tracker/',
   plugins: [react(), tailwindcss()],
-}) 
+})
